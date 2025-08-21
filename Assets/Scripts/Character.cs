f1d5fb013d3b0775f126722cffe84838177b3052
Alexandros Genzor <alexgenz03@gmail.com>
2025-08-21 13:34:09 +0200

    Added AxisTarget and Target to CharReference's Char (also added both scene references to script). Created functions to control character movement, camera and health changes.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,7 +8,6 @@ public class Character : MonoBehaviour
     #region CAMERA
     [Header("-- CAMERA --")]
     [LabelOverride("Camera")] public Camera cam;
-    [LabelOverride("Speed")] public float speed;
     [LabelOverride("Vertical Sensitivity")] public float vSens;
     [LabelOverride("Horizontal Sensitivity")] public float hSens;
 
@@ -18,9 +17,13 @@ public class Character : MonoBehaviour
     [LabelOverride("Camera Target")] public GameObject tgt;
     [LabelOverride("Axis Target")] public GameObject axisTgt;
 
+    [LabelOverride("Camera Trailing")] public float t;
+
     [Header("-- CHARACTER --")] 
-    [LabelOverride("Minimum Health")] public int minHealth;
-    [LabelOverride("Maximum Health")] public int maxHealth;
+    [LabelOverride("Character Speed")] public float speed;
+    
+    [LabelOverride("Minimum Health")] public int minHealth = 0;
+    [LabelOverride("Maximum Health")] public int maxHealth = 100;
 
     private int _health;
     public int Health { get { return _health; } set { _health = Mathf.Clamp(value, minHealth, maxHealth); } }
@@ -33,17 +36,59 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // idk why declare both min and max health here when both are public and serialized :/
+        _health = maxHealth;
         
-        
+        Debug.Log(cam);
+
+        // automatically sets camera as the main one in scene if no camera reference has been dragged to the component.
+        if (cam == null)
+            cam = Camera.main;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        CameraControl();
+
+        if (gotDmg)
+        {
+            AlterHealth(-10);
+            gotDmg = false;
+            
+        }
         
     }
     
     #endregion
+
+    public void AlterHealth(int healthChange)
+    {
+        Health += healthChange;
+        
+    }
+
+    public void Movement()
+    {
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        
+    }
+
+    public void CameraControl()
+    {
+        cam.transform.position =
+            Vector3.Lerp(cam.transform.position, tgt.transform.position, Mathf.SmoothStep(0, 1, t));
+
+        // vRot += Input.GetAxis("Mouse Y") * vSens;
+        hRot += Input.GetAxis("Mouse X") * hSens;
+        
+        axisTgt.transform.eulerAngles = new Vector3(0, hRot, 0);
+        
+        cam.transform.LookAt(transform.position);
+
+    }
     
 }
